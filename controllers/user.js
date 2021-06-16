e653f53f6ec6b7cf4ac75d965516fdb7a022b9ae
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-06-16 11:05:56 +0200

    création d'une connection user enregistré dans la base de donnée

--- controllers/user.js
+++ controllers/user.js
@@ -25,4 +25,23 @@ exports.signup = (req, res, next) => {
 
 // Connection d'utilisateur existant
 exports.login = (req, res, next) => {
+    // Trouve un user dans la base de données
+    User.findOne({ email: req.body.email })
+    .then(user => {
+        if(!user) {
+            return res.status(401).json({ error : 'Utilisateur non trouvé !'});
+        }
+        // Compare le mdp envoyer dansl a requete et le mdp dans la base de donnée
+        bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+            if(!valid) {
+                return res.status(401).json({ error : 'Mot de passe incorrecte !'})
+            }
+            res.status(200).json({
+                userId: user._id,
+                token: 'TOKEN'
+            })
+            });
+    })
+    .catch(error => res.status(500).json({ error }));
 };
