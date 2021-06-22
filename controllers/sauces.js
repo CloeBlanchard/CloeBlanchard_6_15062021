e896516eca1c1e647f21425289e208fd7423cc25
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-06-22 15:54:18 +0200

    modification controllers/sauces.js exports.createThing et exports.modifyThing

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -7,12 +7,22 @@ const fs = require('fs');
 // Export de la fonction createThing et logique métier pour la route post
 exports.createThing = (req, res, next) => {
     // envoyer un fichier avec la requete
-    const thingObject = req.body;
+    const thingObject = JSON.parse(req.body.sauce);
+    // On enlève l'id (il sera généré automatiquement)
+    delete thingObject._id;
     // corps de la requete
     const thing = new Thing({
         ...thingObject,
         // modification de l'url de l'image
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+        // modification de likes
+        likes: 0,
+        // modification de dislikes
+        dislikes: 0,
+        // modification de usersLiked
+        usersLiked: [],
+        // modification de udersDisliked
+        usersDisliked: [],
     });
     // enregistrement de l'objet dans la base de donnée
     thing.save()
@@ -24,10 +34,11 @@ exports.createThing = (req, res, next) => {
 exports.modifyThing = (req, res, next) => {
     const thingObject = req.file ?
         {
-            ...JSON.parse(req.body.thing),
-            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-        } : { ...req.body };
-    Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.body })
+            ...JSON.parse(req.body.sauce),
+            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+        } 
+        : { ...req.body };
+    Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Objet modifié !' }))
         .catch(error => res.status(400).json({ error }));
 };
@@ -52,14 +63,14 @@ exports.deleteThing = (req, res, nexet) => {
 // Export de la fonction getOneThing et logique métier pour la creation d'un objet spécifique
 exports.getOneThing = (req, res, next) => {
     Thing.findOne({ _id: req.params.id })
-        .then(thing => res.status(200).json(thing))
+        .then(thing => res.status(200).json( thing ))
         .catch(error => res.status(404).json({ error })); //Objet non trouvé
 };
 
 // Export de la fonction getAllThings et logique métier pour la creation de tableau d'objet
 exports.getAllThing = (req, res, next) => {
     Thing.find()
-        .then(things => res.status(200).json({ things }))
+        .then(things => res.status(200).json( things ))
         .catch(error => res.status(400).json({ error }));
 };
 
