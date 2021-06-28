3210c3d3e6c75c46bea1127fda837cbc38745fd7
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-06-28 16:05:15 +0200

    mise en forme du document

--- models/user.js
+++ models/user.js
@@ -1,20 +1,16 @@
 // Importation de mongoose
 const mongoose = require('mongoose');
-
 // Importation du validateur unique
 const uniqueValidator = require('mongoose-unique-validator');
 
-
 // Creation user Schema
 const userSchema = mongoose.Schema({
-    email: { type: String, required: true, unique: true},
-    password: { type: String, required: true}
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
 });
-
 
 // Application du plug-in du validateur unique
 userSchema.plugin(uniqueValidator);
-
 
 // Export du schema en model
 module.exports = mongoose.model('user', userSchema);
