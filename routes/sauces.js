ad48151b851052f1f347fea053741e3385533dfa
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-06-28 16:03:59 +0200

    mise en forme du document

--- routes/sauces.js
+++ routes/sauces.js
@@ -1,35 +1,26 @@
 // Ajout d'express au projet
 const express = require('express');
-
 // Création du routeur
 const router = express.Router();
 
-
 // Import du controllers/sauces
 const saucesCtrl = require('../controllers/sauces');
-
 // Import de l'authentification
 const auth = require('../middleware/auth');
-
 // Import du middlewar de gestion de fichier
 const multer = require('../middleware/multer-config');
 
 
 // Creation de la requete post création d'un objet
 router.post('/', auth, multer, saucesCtrl.createSauce);
-
 // Requete PUT modification d'un objet existant
 router.put('/:id', auth, multer, saucesCtrl.modifySauce);
-
 // Requete DELETE suppression d'un objet
 router.delete('/:id', auth, saucesCtrl.deleteSauce);
-
 // Requet GET récupération d'un objet spécifique
 router.get('/:id', auth, saucesCtrl.getOneSauce);
-
 // Requete GET récupération d'un tableau d'objet
 router.get('/', auth, saucesCtrl.getAllSauces);
-
 // Requete POST du like
 router.post('/:id/like', auth, saucesCtrl.voteSauce);
 
--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,14 @@
 // Ajout d'express pour les router
 const express = require('express');
-
 // Import du router express
 const router = express.Router();
-
 // Import du user control
 const userCtrl = require('../controllers/user');
-
 
 // Import des routes post pour l'authentification
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
+
 
 // Export du router
 module.exports = router;
