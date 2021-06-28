d23151849bb880b6018e741ff522f6a75b520f8b
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-06-28 09:52:24 +0200

    changement nom de fichier

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -1,18 +1,18 @@
 // Importation du schema thing
-const Thing = require('../models/thing');
+const Sauce = require('../models/sauces');
 
 // Import de file system
 const fs = require('fs');
 
 // Export de la fonction createThing et logique métier pour la route post
-exports.createThing = (req, res, next) => {
+exports.createSauce = (req, res, next) => {
     // envoyer un fichier avec la requete
-    const thingObject = JSON.parse(req.body.sauce);
+    const sauceObject = JSON.parse(req.body.sauce);
     // On enlève l'id (il sera généré automatiquement)
-    delete thingObject._id;
+    delete sauceObject._id;
     // corps de la requete
-    const thing = new Thing({
-        ...thingObject,
+    const sauce = new Sauce ({
+        ...sauceObject,
         // modification de l'url de l'image
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
         // modification de likes
@@ -25,31 +25,31 @@ exports.createThing = (req, res, next) => {
         usersDisliked: [],
     });
     // enregistrement de l'objet dans la base de donnée
-    thing.save()
+    sauce.save()
         .then(() => res.status(201).json({ message: 'Objet enregistré !' })) // Objet trouvé
         .catch(error => res.status(400).json({ error }));
 };
 
 // Export de la fonction modifyThing et logique métier pour la mise à jour d'un objet existant
-exports.modifyThing = (req, res, next) => {
-    const thingObject = req.file ?
+exports.modifySauce = (req, res, next) => {
+    const sauceObject = req.file ?
         {
             ...JSON.parse(req.body.sauce),
             imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
         } 
         : { ...req.body };
-    Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+        Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Objet modifié !' }))
         .catch(error => res.status(400).json({ error }));
 };
 
 // Export de la fonction deleteThing et logique métier pour la supression d'un objet
-exports.deleteThing = (req, res, nexet) => {
+exports.deleteSauce = (req, res, nexet) => {
     // Récupération de l'objet
-    Thing.findOne({ _id: req.params.id })
-        .then((thing) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then((sauce) => {
             // Récupération du nom du fichier
-            const filename = thing.imageUrl.split('/images/')[1];
+            const filename = sauce.imageUrl.split('/images/')[1];
             // Pour supprimer un fichier
             fs.unlink(`images/${filename}`, () => {
                 Thing.deleteOne({ _id: req.params.id })
@@ -61,32 +61,32 @@ exports.deleteThing = (req, res, nexet) => {
 };
 
 // Export de la fonction getOneThing et logique métier pour la creation d'un objet spécifique
-exports.getOneThing = (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-        .then(thing => res.status(200).json( thing ))
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => res.status(200).json( sauce ))
         .catch(error => res.status(404).json({ error })); //Objet non trouvé
 };
 
 // Export de la fonction getAllThings et logique métier pour la creation de tableau d'objet
-exports.getAllThing = (req, res, next) => {
-    Thing.find()
-        .then(things => res.status(200).json( things ))
+exports.getAllSauces = (req, res, next) => {
+    Sauce.find()
+        .then(sauces => res.status(200).json( sauces ))
         .catch(error => res.status(400).json({ error }));
 };
 
 // Système de like et dislike
 // Exports de la fonction likeDislikeSauce
-exports.voteThing = (req, res, next) => {
+exports.voteSauce = (req, res, next) => {
     // Evalue une expression et celon la response obtiens le cas associé
     switch (req.body.like) {
         // Si le cas correspond à 0
         case 0:
-            Thing.findOne({ _id: req.params.id})
+            Sauce.findOne({ _id: req.params.id})
             .then((sauce) => { 
                 // Système de dislike si le user est déjà dans le tableau de usersLike
                 if (sauce.usersDisliked.find(user => user === req.body.userId)) {
                     // User est dans le tableau alors on update la sauce avec _id de la requete
-                    Thing.updateOne({ _id: req.params.id} , {
+                    Sauce.updateOne({ _id: req.params.id} , {
                         // Décrémentation de 1 des valeurs de Dislike (-1)
                         $inc: { dislikes: -1 },
                         // Le user est retiré du tableau
@@ -98,7 +98,7 @@ exports.voteThing = (req, res, next) => {
                 // Système de like si user est déjà dans le tableau de usersLike
                 if (sauce.usersLiked.find(user => user === req.body.userId)) {
                     // User est dans le tableau alors on update la sauce avec _id de la requete
-                    Thing.updateOne({ _id: req.params.id }, {
+                    Sauce.updateOne({ _id: req.params.id }, {
                         // Décrémentation de 1 des valeurs de Like (-1)
                         $inc: { likes: -1},
                         // Le user est retiré du tableau
@@ -116,7 +116,7 @@ exports.voteThing = (req, res, next) => {
         // Si le cas correspond a 1
         case 1:
             // Recherche ed la sauce correspondant à _id
-            Thing.updateOne({ _id: req.params.id}, {
+            Sauce.updateOne({ _id: req.params.id}, {
                 // Incrémentation de 1 des valeurs de Like (+1)
                 $inc: { likes: 1},
                 // Ajout de le user dans le tableau 'usersLiked'
@@ -127,10 +127,10 @@ exports.voteThing = (req, res, next) => {
             // On rompt la boucle
             break;
         // Système de dislike si user n'est pas dans le tableau 
-        // Si le cas correspond a 1
+        // Si le cas correspond a -1
         case -1:
             // Recherche ed la sauce correspondant à _id
-            Thing.updateOne({ _id: req.params.id}, {
+            Sauce.updateOne({ _id: req.params.id}, {
                 // Incrémentation de 1 des valeurs de Like (+1)
                 $inc: { dislikes: 1},
                 // Ajout de le user dans le tableau 'usersLiked'
@@ -141,7 +141,7 @@ exports.voteThing = (req, res, next) => {
             // On rompt la boucle
             break;
         // Erreur par defaut
-        default:
-        console.error('Bad request');
+            default:
+            console.error('Bad request');
     }
 };
