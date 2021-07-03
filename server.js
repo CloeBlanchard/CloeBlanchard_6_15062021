f544be907cad421b3d05987b450a1b6bf68ded3e
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-07-04 01:10:10 +0200

    mise en forme du document

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const http = require('http');
 // importation de l'application
 const app = require('./app');
 
-// la fonction normalizePort renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne
+// la fonction normalizePort renvoie un port valide, qu'il soit fourni sous la forme 
+// d'un numéro ou d'une chaîne
 const normalizePort = val => {
   const port = parseInt(val, 10);
 
@@ -21,7 +22,8 @@ const port = normalizePort(process.env.PORT || '3000');
 // information sur le port utilisé
 app.set('port', port);
 
-// la fonction errorHandler  recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur
+// la fonction errorHandler  recherche les différentes erreurs et les gère de manière appropriée. 
+// La fonction est ensuite enregistrée dans le serveur
 const errorHandler = error => {
   if (error.syscall !== 'listen') {
     throw error;
@@ -47,12 +49,13 @@ const server = http.createServer(app);
 
 server.on('error', errorHandler);
 
-// écouteur d'évènements est également enregistré, consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console
+// écouteur d'évènements est également enregistré, consignant le port ou le canal nommé 
+// sur lequel le serveur s'exécute dans la console
 server.on('listening', () => {
   const address = server.address();
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
   console.log('Listening on ' + bind);
 });
 
-// Ecoute du serveur avec num du port ecouter
+// Ecoute du serveur avec numéro du port ecouter
 server.listen(port);
